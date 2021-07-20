c0914ad0d8758e1bc5e3059889ad943b5a07f346
Joey_M_Brown <joey.ma.brown@gmail.com>
2021-07-20 14:47:15 -0400

    Finished project

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,19 +1,25 @@
 import React from 'react'
 // We'll need quite a few imports from react-router-dom
-
+import { useParams, NavLink, useRouteMatch, Route } from 'react-router-dom'
 import ItemDetails from './ItemDetails'
 
 export default function Item(props) {
   // We get ALL items through props. We'll use the URL to find out which item is the one to show.
   const { items } = props
+  const { itemId } = useParams();
+  const { url, path } = useRouteMatch();
+
+  const item = items.find(function (item) {
+    return item.id === (parseInt(itemId)) ? item : false;
+  })
 
   // 👉 STEP 7 - We need to pull item from items, using a parameter in the URL (:itemID)
   // Beware! The ids are integers, whereas URL parameters are strings.
   // Beware! The JSX is expecting 'item' to exist instantly!
   // we use this hook to grab they dynamic parts of the path (:itemID).
-  const item = {}
 
   return (
+    !item ? <h2>Loading...</h2> : 
     <div className='item-wrapper'>
       <div className='item-header'>
         <div className='image-wrapper'>
@@ -27,10 +33,20 @@ export default function Item(props) {
 
       <nav className='item-sub-nav'>
         {/* 👉 STEP 8 - Here go the NavLinks to `<current url>/shipping` and `<current url>/description` */}
+        <NavLink  to={`${url}/shipping`}>Shipping</NavLink>
+        <NavLink  to={`${url}/description`}>Description</NavLink>
       </nav>
 
       {/* 👉 STEP 9 - Here go the Routes for `<current path>/shipping` and `<current path>/description` */}
       {/* These Routes should render <ItemDetails /> */}
+      <Route path={`${path}/shipping`}>
+        <ItemDetails text={item.shipping} />
+      </Route>
+
+      <Route path={`${path}/description`}>
+        <ItemDetails text={item.description} />
+      </Route>
+
 
       {/* 👉 STEP 10 - Shorten paths and urls with `useRouteMatch` hook */}
     </div>
--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -1,10 +1,13 @@
 import React from 'react'
 // We'll need a Link and the useRouteMatch hook from 'react-router-dom'
+import { Link, useRouteMatch } from 'react-router-dom';
 
 export default function ItemsList(props) {
   const { items } = props
 
   // We'll grab the current URL using the hook
+  const { url } = useRouteMatch();
+  //console.log(location);
 
   return (
     <div className='items-list-wrapper'>
@@ -13,13 +16,15 @@ export default function ItemsList(props) {
           className='item-card'
           key={item.id}
         >
-          {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
-          <img
-            className='items-list-image'
-            src={item.imageUrl}
-            alt={item.name}
-          />
-          <p>{item.name}</p>
+          {/*<Link to={`${location.pathname}/${item.id}`}></Link>*//* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
+          <Link to={`${url}/${item.id}`}>
+            <img
+              className='items-list-image'
+              src={item.imageUrl}
+              alt={item.name}
+            />
+            <p>{item.name}</p>
+          </Link>
           {/* Link ends */}
 
           <p>${item.price}</p>
